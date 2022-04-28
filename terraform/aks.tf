ee134a8a4138cdbdc9bee4e9afad3c65e0dcf762
Matt Jeanes <matthew.jeanes@myairops.com>
2022-04-28 03:34:12 +0100

    basic terraform setup

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -2,4 +2,17 @@ resource "azurerm_kubernetes_cluster" "abyss" {
   name = "abyss"
   resource_group_name = azurerm_resource_group.abyss.name
   location = azurerm_resource_group.abyss.location
+  dns_prefix = "abyss-dns"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  default_node_pool {
+    name       = "default"
+    enable_auto_scaling = true
+    min_count = 1
+    max_count = 5
+    vm_size    = "Standard_B2s"
+  }
 }
