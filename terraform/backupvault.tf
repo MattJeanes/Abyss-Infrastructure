f87ebe1c9b5b02d8b94332f0b11cf7eed8b6210c
Matt Jeanes <mattjeanes23@gmail.com>
2022-06-20 02:26:43 +0100

    setup backup vault for important disks + add mongodb

---
+++ terraform/backupvault.tf
@@ -0,0 +1,37 @@
+resource "azurerm_resource_group" "snapshots" {
+  name     = "abyss_snapshots"
+  location = "northeurope"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_data_protection_backup_vault" "main" {
+  name                = "backup-vault"
+  resource_group_name = azurerm_resource_group.abyss.name
+  location            = azurerm_resource_group.abyss.location
+  datastore_type      = "VaultStore"
+  redundancy          = "LocallyRedundant"
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_role_assignment" "snapshots_disk_snapshot_contributor" {
+  scope                = azurerm_resource_group.snapshots.id
+  role_definition_name = "Disk Snapshot Contributor"
+  principal_id         = azurerm_data_protection_backup_vault.main.identity[0].principal_id
+}
+
+resource "azurerm_data_protection_backup_policy_disk" "main" {
+  name     = "disk-backup-policy"
+  vault_id = azurerm_data_protection_backup_vault.main.id
+
+  backup_repeating_time_intervals = ["R/2022-01-01T07:00:00+00:00/P1D"]
+  default_retention_duration      = "P7D"
+
+  depends_on = [
+    azurerm_role_assignment.snapshots_disk_snapshot_contributor
+  ]
+}
--- terraform/teamspeak.tf
+++ terraform/teamspeak.tf
@@ -16,6 +16,25 @@ output "teamspeak_disk_id" {
   sensitive = true
 }
 
+resource "azurerm_role_assignment" "teamspeak_disk_backup_reader" {
+  scope                = azurerm_managed_disk.teamspeak.id
+  role_definition_name = "Disk Backup Reader"
+  principal_id         = azurerm_data_protection_backup_vault.main.identity[0].principal_id
+}
+
+resource "azurerm_data_protection_backup_instance_disk" "teamspeak" {
+  name                         = azurerm_managed_disk.teamspeak.name
+  disk_id                      = azurerm_managed_disk.teamspeak.id
+  location                     = azurerm_data_protection_backup_vault.main.location
+  vault_id                     = azurerm_data_protection_backup_vault.main.id
+  snapshot_resource_group_name = azurerm_resource_group.snapshots.name
+  backup_policy_id             = azurerm_data_protection_backup_policy_disk.main.id
+
+  depends_on = [
+    azurerm_role_assignment.teamspeak_disk_backup_reader
+  ]
+}
+
 resource "azurerm_managed_disk" "teamspeak_alt" {
   name                 = "teamspeak-alt"
   location             = azurerm_resource_group.abyss.location
@@ -50,4 +69,23 @@ resource "azurerm_managed_disk" "sinusbot" {
 output "sinusbot_disk_id" {
   value     = azurerm_managed_disk.sinusbot.id
   sensitive = true
+}
+
+resource "azurerm_role_assignment" "sinusbot_disk_backup_reader" {
+  scope                = azurerm_managed_disk.sinusbot.id
+  role_definition_name = "Disk Backup Reader"
+  principal_id         = azurerm_data_protection_backup_vault.main.identity[0].principal_id
+}
+
+resource "azurerm_data_protection_backup_instance_disk" "sinusbot" {
+  name                         = azurerm_managed_disk.sinusbot.name
+  disk_id                      = azurerm_managed_disk.sinusbot.id
+  location                     = azurerm_data_protection_backup_vault.main.location
+  vault_id                     = azurerm_data_protection_backup_vault.main.id
+  snapshot_resource_group_name = azurerm_resource_group.snapshots.name
+  backup_policy_id             = azurerm_data_protection_backup_policy_disk.main.id
+
+  depends_on = [
+    azurerm_role_assignment.sinusbot_disk_backup_reader
+  ]
 }
