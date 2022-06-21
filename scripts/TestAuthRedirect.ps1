262ac6f868edf31b41b6520ebba572deb4694b8f
Matt Jeanes <mattjeanes23@gmail.com>
2022-06-21 06:48:45 +0100

    add checks for authentication - very important these do not accidentally fall out

---
+++ scripts/TestAuthRedirect.ps1
@@ -0,0 +1,17 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$Url
+)
+
+. "$PSScriptRoot/Common/Helpers.ps1"
+
+$request = [System.Net.WebRequest]::Create($Url)
+$request.AllowAutoRedirect = $false
+$response = $request.GetResponse()
+
+if ($response.StatusCode -eq [System.Net.HttpStatusCode]::Redirect -and $response.Headers["Location"].StartsWith("https://auth.")) {
+    Write-Host "Authentication redirect working for $Url"
+}
+else {
+    Write-Error "Authentication redirect not working for $Url"
+}
