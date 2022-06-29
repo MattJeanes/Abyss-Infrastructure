5214efdff01eb57391c4585c8e2cba23f7dc0d49
Matt Jeanes <mattjeanes23@gmail.com>
2022-06-29 01:37:11 +0100

    create recovery vault for CDN

---
+++ terraform/recoveryvault.tf
@@ -0,0 +1,22 @@
+resource "azurerm_recovery_services_vault" "vault" {
+  name                = "recovery-vault"
+  location            = azurerm_resource_group.abyss.location
+  resource_group_name = azurerm_resource_group.abyss.name
+  sku                 = "Standard"
+  storage_mode_type   = "LocallyRedundant"
+}
+
+resource "azurerm_backup_policy_file_share" "vault" {
+  name                = "recovery-vault-policy"
+  resource_group_name = azurerm_resource_group.abyss.name
+  recovery_vault_name = azurerm_recovery_services_vault.vault.name
+
+  backup {
+    frequency = "Daily"
+    time      = "07:00"
+  }
+
+  retention_daily {
+    count = 7
+  }
+}
--- terraform/storageaccount.tf
+++ terraform/storageaccount.tf
@@ -11,6 +11,12 @@ resource "azurerm_storage_account" "abyss" {
   }
 }
 
+resource "azurerm_backup_container_storage_account" "abyss" {
+  resource_group_name = azurerm_resource_group.abyss.name
+  recovery_vault_name = azurerm_recovery_services_vault.vault.name
+  storage_account_id  = azurerm_storage_account.abyss.id
+}
+
 resource "azurerm_storage_share" "abyss_models" {
   name                 = "models"
   storage_account_name = azurerm_storage_account.abyss.name
@@ -59,6 +65,14 @@ resource "azurerm_storage_share" "abyss_cdn" {
   lifecycle {
     prevent_destroy = true
   }
+}
+
+resource "azurerm_backup_protected_file_share" "abyss_cdn" {
+  resource_group_name       = azurerm_resource_group.abyss.name
+  recovery_vault_name       = azurerm_recovery_services_vault.vault.name
+  source_storage_account_id = azurerm_backup_container_storage_account.abyss.storage_account_id
+  source_file_share_name    = azurerm_storage_share.abyss_cdn.name
+  backup_policy_id          = azurerm_backup_policy_file_share.vault.id
 }
 
 output "storage_account_name" {
