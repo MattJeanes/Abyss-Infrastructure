8a385015b0293c09593d3b5a2d411134bb055b0f
Matt Jeanes <mattjeanes23@gmail.com>
2023-02-01 01:49:55 +0000

    switch teamspeak, teamspeak alt and sinusbot to use file shares

--- terraform/storageaccount.tf
+++ terraform/storageaccount.tf
@@ -75,6 +75,44 @@ resource "azurerm_backup_protected_file_share" "abyss_cdn" {
   backup_policy_id          = azurerm_backup_policy_file_share.vault.id
 }
 
+resource "azurerm_storage_share" "abyss_teamspeak" {
+  name                 = "teamspeak"
+  storage_account_name = azurerm_storage_account.abyss.name
+  quota                = "10"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_backup_protected_file_share" "abyss_teamspeak" {
+  resource_group_name       = azurerm_resource_group.abyss.name
+  recovery_vault_name       = azurerm_recovery_services_vault.vault.name
+  source_storage_account_id = azurerm_backup_container_storage_account.abyss.storage_account_id
+  source_file_share_name    = azurerm_storage_share.abyss_teamspeak.name
+  backup_policy_id          = azurerm_backup_policy_file_share.vault.id
+}
+
+resource "azurerm_storage_share" "abyss_teamspeak_alt" {
+  name                 = "teamspeak-alt"
+  storage_account_name = azurerm_storage_account.abyss.name
+  quota                = "10"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "azurerm_storage_share" "abyss_sinusbot" {
+  name                 = "sinusbot"
+  storage_account_name = azurerm_storage_account.abyss.name
+  quota                = "10"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
 output "storage_account_name" {
   value     = azurerm_storage_account.abyss.name
   sensitive = true
