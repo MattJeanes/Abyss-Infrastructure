e0c97af6782af126fe55ef25e25bb7321ecc2bb2
Matt Jeanes <mattjeanes23@gmail.com>
2023-11-11 08:00:31 +0000

    small cleanup

--- terraform/qbittorrent.tf
+++ terraform/qbittorrent.tf
@@ -7,13 +7,3 @@ resource "azurerm_storage_share" "abyss_qbittorrent_config" {
     prevent_destroy = true
   }
 }
-
-resource "azurerm_storage_share" "abyss_qbittorrent_downloads" {
-  name                 = "qbittorrent-downloads"
-  storage_account_name = azurerm_storage_account.abyss.name
-  quota                = 100
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
