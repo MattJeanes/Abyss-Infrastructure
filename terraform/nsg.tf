50917fd0dbfbd1fc353d502a4e357d922d65ec02
Matt Jeanes <mattjeanes23@gmail.com>
2023-11-11 08:58:15 +0000

    remove no longer needed 6881 route as handled by VPN now

--- terraform/nsg.tf
+++ terraform/nsg.tf
@@ -78,18 +78,3 @@ resource "azurerm_network_security_rule" "abyss_teamspeak_alt" {
   destination_address_prefix = "*"
   destination_port_ranges    = ["9988", "10012", "30034"]
 }
-
-resource "azurerm_network_security_rule" "abyss_qbittorrent" {
-  network_security_group_name = azurerm_network_security_group.abyss.name
-  resource_group_name         = azurerm_network_security_group.abyss.resource_group_name
-
-  name                       = "qBittorrent"
-  priority                   = 342
-  direction                  = "Inbound"
-  access                     = "Allow"
-  protocol                   = "*"
-  source_address_prefix      = "*"
-  source_port_range          = "*"
-  destination_address_prefix = "*"
-  destination_port_range    = "6881"
-}
